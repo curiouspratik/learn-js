398986f97ec43d05c253f1a49bc4d7a710cf7ebb
Pratik Patil <patil.pratiq@gmail.com>
2024-02-03 23:44:18 -0500

    added changes from the lecture

--- quiz/scripts/hideEmail-q.js
+++ quiz/scripts/hideEmail-q.js
@@ -5,7 +5,20 @@ const emails = ['mary@northeastern.edu', 'ari@khoury.northeastern.edu', 'jk_neu.
 window.onload = function hideEmail() {
     const list = document.getElementById('emails')
     list.innerHTML = '';
+    result = []
+    const ulElement = document.getElementById("emails");
     for (const email of emails) {
         // complete the loop
+        if(isValidEmail(email)) {
+            hiddenEmail = email.split('@')[0].replace(/./g,'*')+ '@' + email.split('@')[1];
+            const liElement = document.createElement('li');
+            liElement.innerText = hiddenEmail;
+            ulElement.append(liElement);
+        }
     }
+    
+}
+
+function isValidEmail(email) {
+    return /^[a-zA-Z_0-9]+@(.+\.)*northeastern\.edu/.test(email);
 }
--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,19 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+logger = {
+    logMsg: function() {
+        if(this.hasOwnProperty('errMsg')) {
+            console.log(`Error message: ${this.errMsg}`);
+        }
+    }
+}
+
+logObj1 = {
+    errMsg: 'This is error 1'
+}
+
+logObj2 = {
+    errMsg: 'This is error 2'
+}
+
+logger.logMsg.call(logObj1)
+logger.logMsg.call(logObj2)
